817e57a35befc2145a67e7bb0e2c797a0a4031e1
Yuri Wallace <yuri512w@gmail.com>
2023-06-27 22:20:51 -0300

    blog

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title": "1",
+    "description": "teste",
+    "photoCover":""
+  },
+  {
+    "id":"2",
+    "title": "2",
+    "description": "bla blabla",
+    "photoCover":""
+  }
+]
